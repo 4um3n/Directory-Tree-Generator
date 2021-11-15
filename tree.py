982fd7da298fb5d0e68dc49871f128ac5eb2d735
Rumen Grozdanov <89039290+4um3n@users.noreply.github.com>
2021-11-15 17:26:45 +0200

    Delete tree.py

--- tree.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# tree.py
-
-"""This module provides Directory Tree Generator entry point CLI script."""
-
-from functionality.cli import main
-
-if __name__ == "__main__":
-    main()
